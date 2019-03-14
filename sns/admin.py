961c6741231a22c9812a8a67a6caaaae43f0c247
hanaluana <hanaluana23@gmail.com>
2019-03-14 14:23:01 +0900

    finish Comment model

--- sns/admin.py
+++ sns/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Posting
+from .models import Posting, Comment
 
 # Register your models here.
 class PostingModelAdmin(admin.ModelAdmin):
@@ -8,3 +8,10 @@ class PostingModelAdmin(admin.ModelAdmin):
     list_display_links = ('id','content') # 리스트에서 click 해서 들어갈 수 있는 column
 
 admin.site.register(Posting, PostingModelAdmin)
+
+class CommentModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')  # 개별 정보 화면
+    list_display = ('id', 'content', 'created_at', 'updated_at')  # 전체 리스트 화면
+    list_display_links = ('id', 'content')  # 리스트에서 click 해서 들어갈 수 있는 column
+
+admin.site.register(Comment, CommentModelAdmin)
